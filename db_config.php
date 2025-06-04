b43797bd6d53153c38caee8e5cfe3be56468488a
vaishnavi2005-ux <vaishnavip2005@outlook.com>
2025-06-04 16:20:26 +0200

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -1,9 +1,9 @@
 <?php
-// MySQL connection
+
 $mysqli = new mysqli("localhost", "username", "password", "elderly_care_sql");
 
-// MongoDB connection
-require 'vendor/autoload.php'; // Use Composer to install MongoDB PHP library
+
+require 'vendor/autoload.php';
 $mongoClient = new MongoDB\Client("mongodb+srv://vaishnavi2005:vaishnavi@cluster0.ddzlgcu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 $mongoCollection = $mongoClient->elderly_care->sensor_data;
 ?>
